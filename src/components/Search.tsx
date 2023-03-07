2f35105291ce72944c70b1081b17fc9265527762
charafmrah <charaf4charaf@gmail.com>
2023-03-07 16:40:56 +0300

    navbar

--- src/components/Search.tsx
+++ src/components/Search.tsx
@@ -1,10 +1,13 @@
 const Search: React.FC = () => {
   return (
-    <form className="form-control">
+    <form className="form-control flex w-full max-w-md flex-col gap-5">
+      <label className="label">Enter the ingredients</label>
       <textarea
         className="textarea-bordered textarea rounded-sm"
         placeholder="Bio"
       ></textarea>
+
+      <button className="btn-secondary btn rounded-md">Generate recipe</button>
     </form>
   );
 };
--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -2,9 +2,9 @@ import { Html, Head, Main, NextScript } from "next/document";
 
 export default function Document() {
   return (
-    <Html lang="en" data-theme="cupcake">
+    <Html data-theme="cupcake">
       <Head />
-      <body>
+      <body className="h-full">
         <Main />
         <NextScript />
       </body>
