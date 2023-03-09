751c892db3404d76b09783359be5ef25317f2708
charafmrah <charaf4charaf@gmail.com>
2023-03-10 00:29:48 +0300

    bug fix

--- src/components/Search.tsx
+++ src/components/Search.tsx
@@ -7,10 +7,6 @@ import ReactMarkdown from "react-markdown";
 import Link from "next/link";
 
 const Search: React.FC = () => {
-  const { data: sessionData } = useSession();
-
-  const generateRecipe = api.recipe.generate.useMutation({});
-
   const [recipes, setRecipes] = useState<string>("");
   const [loading, setLoading] = useState<boolean>(false);
   const ingredientsRef = useRef<HTMLTextAreaElement>(null);
@@ -36,41 +32,28 @@ const Search: React.FC = () => {
 
   return (
     <>
-      {sessionData?.user ? (
-        <>
-          <form
-            // eslint-disable-next-line @typescript-eslint/no-misused-promises
-            onSubmit={handleSubmit}
-            method="post"
-            className="form-control flex w-full max-w-md flex-col gap-5"
-          >
-            <label className="label">
-              What ingredients do you have available?
-            </label>
-            <textarea
-              ref={ingredientsRef}
-              className="textarea-bordered textarea rounded-md lg:min-h-[50vh]"
-              placeholder="Tomatoes, onions, garlic, olive oil, salt, pepper, etc."
-            ></textarea>
+      <form
+        // eslint-disable-next-line @typescript-eslint/no-misused-promises
+        onSubmit={handleSubmit}
+        method="post"
+        className="form-control flex w-full max-w-md flex-col gap-5"
+      >
+        <label className="label">What ingredients do you have available?</label>
+        <textarea
+          ref={ingredientsRef}
+          className="textarea-bordered textarea rounded-md lg:min-h-[50vh]"
+          placeholder="Tomatoes, onions, garlic, olive oil, salt, pepper, etc."
+        ></textarea>
 
-            <button className="btn-secondary btn rounded-md">
-              Generate recipe
-            </button>
-          </form>
-          <article className="prose">
-            <h2>Genie Generated Recipe:</h2>
+        <button className="btn-secondary btn rounded-md">
+          Generate recipe
+        </button>
+      </form>
+      <article className="prose">
+        <h2>Genie Generated Recipe:</h2>
 
-            <ReactMarkdown>{recipes}</ReactMarkdown>
-          </article>
-        </>
-      ) : (
-        <div className="flex flex-col items-center justify-center gap-5 text-center">
-          <h1 className="text-5xl font-bold">You need to be signed in!</h1>
-          <Link onClick={void signIn()} href="" className="justify-between">
-            <button className="btn-primary btn rounded-md">Sign in</button>
-          </Link>
-        </div>
-      )}
+        <ReactMarkdown>{recipes}</ReactMarkdown>
+      </article>
     </>
   );
 };
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,11 +2,13 @@ import { type NextPage } from "next";
 import Head from "next/head";
 import { signIn, signOut, useSession } from "next-auth/react";
 
-import { api } from "~/utils/api";
 import Navbar from "~/components/Navbar";
 import Search from "~/components/Search";
+import Link from "next/link";
 
 const Home: NextPage = () => {
+  const { data: sessionData } = useSession();
+
   return (
     <>
       <Head>
@@ -17,7 +19,22 @@ const Home: NextPage = () => {
       <Navbar />
       <main className="w-full">
         <div className="flex h-full flex-wrap items-start justify-center gap-20 py-10 px-5 lg:gap-32 lg:py-20">
-          <Search />
+          {sessionData?.user ? (
+            <Search />
+          ) : (
+            <div className="flex flex-col items-center justify-center gap-5 text-center">
+              <h1 className="text-5xl font-bold">You need to be signed in!</h1>
+              <Link
+                onClick={(event) => {
+                  event.preventDefault();
+                  void signIn();
+                }}
+                href="/api/auth/"
+              >
+                <button className="btn-primary btn rounded-md">Sign in</button>
+              </Link>
+            </div>
+          )}
         </div>
       </main>
     </>
